db86f0e1182300c57244dbf1e60cda3202e75960
adurhmn <abdurmrahman7@gmail.com>
2022-06-02 01:55:13 +0530

    added projects tab

---
+++ static/js/index.js
@@ -0,0 +1,38 @@
+const projectBtn = document.getElementById("projects");
+const homeBtn = document.querySelector(".backToHome");
+const profileContent = document.querySelector(".profile-content");
+const projectContent = document.querySelector(".project-content");
+const fadeOutClass = "fadeOutElement";
+const fadeInClass = "fadeInElement";
+const hideClass = "hideElement";
+
+projectBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  profileContent.classList.remove(fadeInClass);
+  profileContent.offsetWidth; // forces the browser to reapply animation
+  // https://stackoverflow.com/questions/60686489/what-purpose-does-void-element-offsetwidth-serve
+  // https://css-tricks.com/restart-css-animation/#aa-update-another-javascript-method-to-restart-a-css-animation
+  profileContent.classList.add(fadeOutClass);
+  setTimeout(() => {
+    profileContent.classList.add(hideClass);
+  }, 700);
+
+  projectContent.classList.remove(hideClass);
+  projectContent.classList.remove(fadeOutClass);
+  projectContent.offsetWidth;
+  projectContent.classList.add(fadeInClass);
+});
+
+homeBtn.addEventListener("click", () => {
+  projectContent.classList.remove(fadeInClass);
+  projectContent.offsetWidth;
+  projectContent.classList.add(fadeOutClass);
+  setTimeout(() => {
+    projectContent.classList.add(hideClass);
+  }, 700);
+
+  profileContent.classList.remove(hideClass);
+  profileContent.classList.remove(fadeOutClass);
+  profileContent.offsetWidth;
+  profileContent.classList.add(fadeInClass);
+});
